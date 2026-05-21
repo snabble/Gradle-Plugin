10cda36305d458954a9019678e6dfdd174279646
Fabian Bender <fabian.bender@snabble.io>
2026-05-21 09:04:47 +0200

    remove deprecated api usage and migrate

--- gradle-plugin/src/main/kotlin/io/snabble/setup/SnabbleExtension.kt
+++ gradle-plugin/src/main/kotlin/io/snabble/setup/SnabbleExtension.kt
@@ -1,20 +1,13 @@
 package io.snabble.setup
 
-import com.android.build.gradle.AppExtension
-import com.android.build.gradle.BaseExtension
-import com.android.build.gradle.LibraryExtension
+import com.android.build.api.dsl.ApplicationExtension
+import com.android.build.api.variant.AndroidComponentsExtension
 import org.gradle.api.Action
-import org.gradle.api.GradleException
 import org.gradle.api.Project
-import java.io.File
 import java.lang.IllegalStateException
 
 open class SnabbleExtension(private val project: Project) {
     val environments: Map<Environment, BuildEnvironment> = mutableMapOf()
-    private val baseDir = File(
-        project.buildDir,
-        "generated/snabble"
-    )
 
     private fun getEnvironment(environment: Environment) =
         (environments as MutableMap<Environment, BuildEnvironment>).getOrPut(environment) {
@@ -43,11 +36,11 @@ open class SnabbleExtension(private val project: Project) {
             DownloadTask::class.java
         ) {
             val appId = extension.appId ?: throw IllegalStateException("You must define the app id in order to download the manifest")
-            val app = project.extensions.getByType(BaseExtension::class.java) as AppExtension
+            val app = project.extensions.getByType(ApplicationExtension::class.java)
             val appVersion = app.defaultConfig.versionName ?: throw IllegalStateException("No app version number detected")
             it.url.set("${environment.baseUrl}/metadata/app/$appId/android/$appVersion")
-            val path = "snabble_${environment.name.lowercase()}_metadata.json"
-            it.outputFile = File(baseDir, "res/raw/$path")
+            it.environmentName.set(environment.name.lowercase())
+            it.outputDir.set( project.layout.buildDirectory.dir("generated/snabble/res"))
         }
 
         val generateConfigTask = project.tasks.register(
@@ -69,36 +62,34 @@ open class SnabbleExtension(private val project: Project) {
             it.lastSeenThreshold.set(extension.lastSeenThreshold)
             it.networkInterceptor.set(extension.networkInterceptor)
             it.manualProductDatabaseUpdates.set(extension.manualProductDatabaseUpdates)
-            it.configFile = File(baseDir, "res/raw/snabble_${environment.name.lowercase()}_config.properties")
+            it.environmentName.set(environment.name.lowercase())
+            it.outputDir.set( project.layout.buildDirectory.dir("generated/snabble/res"))
         }
 
-        project.extensions.getByType(BaseExtension::class.java).forEachVariant { variant ->
-            // generate config{Environment}.properties
-            project.tasks.getByName("generate${variant.capitalizedName()}Resources").dependsOn += generateConfigTask
-            // prefetch meta data
+
+        val androidComponents = project.extensions.getByType(AndroidComponentsExtension::class.java)
+        val app = project.extensions.getByType(ApplicationExtension::class.java)
+
+        androidComponents.onVariants { variant ->
             val offline = project.gradle.startParameter.isOffline
-            val debug = variant.buildType.isDebuggable
-            if (!(offline && debug) && extension.prefetchMetaData) {
-                project.tasks.getByName("generate${variant.capitalizedName()}Resources").dependsOn += downloadTask
-            }
-            variant.registerGeneratedResFolders(project.files(File(baseDir, "res")))
-        }
-    }
 
-    @Suppress("DEPRECATION") // For BaseVariant should be replaced in later studio versions
-    private fun BaseExtension.forEachVariant(action: (com.android.build.gradle.api.BaseVariant) -> Unit) {
-        when (this) {
-            is AppExtension -> applicationVariants.all(action)
-            is LibraryExtension -> {
-                libraryVariants.all(action)
-            }
-            else -> throw GradleException(
-                "deeplink builder plugin must be used with android app, library or feature plugin"
+            val isDebuggable = app.buildTypes
+                .getByName(variant.buildType ?: "release")
+                .isDebuggable
+
+            val shouldPrefetch = !(offline && isDebuggable) && extension.prefetchMetaData
+
+            variant.sources.res?.addGeneratedSourceDirectory(
+                generateConfigTask,
+                GenerateSnabbleConfigTask::outputDir
             )
+
+            if (shouldPrefetch) {
+                variant.sources.res?.addGeneratedSourceDirectory(
+                    downloadTask,
+                    DownloadTask::outputDir
+                )
+            }
         }
     }
-
-    @Suppress("DEPRECATION") // For BaseVariant should be replaced in later studio versions
-    private fun com.android.build.gradle.api.BaseVariant.capitalizedName() =
-        Character.toUpperCase(name[0]) + name.substring(1)
 }
